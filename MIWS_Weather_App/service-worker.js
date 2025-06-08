08bcc02dcae502e1b0ccfd1abe8ec5bccc79c22f
tysonia83 <85952816+tysonia83@users.noreply.github.com>
2025-06-08 17:06:42 -0500

    Update service-worker.js

--- MIWS_Weather_App/service-worker.js
+++ MIWS_Weather_App/service-worker.js
@@ -1,1 +1,20 @@
-// Service worker placeholder
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('miws-cache').then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/manifest.json',
+        '/icon.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
